1cbe06195fb387177ae53c1a3a76f37bad31be92
xincy22 <xincy22@mails.tsinghua.edu.cn>
2026-03-22 23:38:28 +0800

    docs: refresh published site

--- assets/core.md.BN7Th6i-.lean.js
+++ assets/core.md.BN7Th6i-.lean.js
@@ -3,7 +3,7 @@ const __pageData = JSON.parse('{"title":"Core 模块架构","description":"","fr
 const _sfc_main = { name: "core.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 47)
+    createStaticVNode("", 46)
   ])]);
 }
 const core = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
--- assets/sdk.md.5HH7CY7M.lean.js
+++ assets/sdk.md.5HH7CY7M.lean.js
@@ -3,7 +3,7 @@ const __pageData = JSON.parse('{"title":"SDK 开发者指南","description":"","
 const _sfc_main = { name: "sdk.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 84)
+    createStaticVNode("", 85)
   ])]);
 }
 const sdk = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
